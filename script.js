d4ec50c4bc677b2a17c408afc762b117de117df8
adi1111 <holubeckiadrian@gmail.com>
2022-06-30 20:42:52 +0200

    improving responsivness

--- script.js
+++ script.js
@@ -10,15 +10,22 @@ function check(e){
         button.disabled = true;
         password.setAttribute('class', 'invalid');
         passwordConfirmation.setAttribute('class', 'invalid');
-
+        information.setAttribute('id', 'warning');
+        information.textContent = 'Password must be the same';  
     } else {
         button.disabled = false;
         password.removeAttribute('class');
         passwordConfirmation.removeAttribute('class');
+        information.textContent = '';
     }
 
 
 }
+const lastRow = document.querySelector('#last-row');
+const information = document.createElement('p');
+const login = document.getElementById('login');
+lastRow.appendChild(information);
 
 
 passwordConfirmation.addEventListener('input', check);
+password.addEventListener('input', check);
